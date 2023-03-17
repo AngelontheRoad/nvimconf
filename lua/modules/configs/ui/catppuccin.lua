c7beaca6d9fd04f19d2101a0796c33e0de88b5a0
AngelontheRoad <1227466340@qq.com>
2023-03-17 11:01:27 +0800

    merge upstream

--- lua/modules/configs/ui/catppuccin.lua
+++ lua/modules/configs/ui/catppuccin.lua
@@ -220,7 +220,8 @@ return function()
 
 					["@field.lua"] = { fg = cp.lavender },
 					["@constructor.lua"] = { fg = cp.flamingo },
-
+					["@variable.builtin.lua"] = { fg = cp.flamingo, style = { "italic" } },
+					
 					["@constant.java"] = { fg = cp.teal },
 
 					["@property.typescript"] = { fg = cp.lavender, style = { "italic" } },
---
+++ lua/overseer/template/c/clang_build.lua
@@ -0,0 +1,16 @@
+return {
+	name = "clang build",
+	builder = function()
+		-- Full path to current file (see :help expand())
+		local file = vim.fn.expand("%:p")
+		local exe_file = vim.fn.expand("%:r")
+		return {
+			cmd = { "clang" },
+			args = { file, "-o", exe_file },
+			components = { { "on_output_quickfix", open = true }, "default" },
+		}
+	end,
+	condition = {
+		filetype = { "c" },
+	},
+}
