b9deae152d96e95feffea68fabf76854133b8674
AngelontheRoad <1227466340@qq.com>
2023-04-26 07:31:12 +0800

    fix: edge and everforest transparent bg setting
    Signed-off-by: AngelontheRoad <1227466340@qq.com>

--- lua/modules/configs/ui/everforest.lua
+++ lua/modules/configs/ui/everforest.lua
@@ -4,6 +4,6 @@ return function()
 	vim.g.everforest_disable_italic_comment = 1
 	vim.g.everforest_show_eob = 1
 	vim.g.everforest_better_performance = 1
-	vim.g.everforest_transparent_background = require("core.settings").transparent_background
+	vim.g.everforest_transparent_background = require("core.settings").transparent_background and 2 or 0
 	vim.g.everforest_dim_inactive_window = 1
 end
