a431f2830bc88d27e5a74f08869e8311a06b435f
AngelontheRoad <gjr2012477@gmail.com>
2024-11-04 03:23:50 +0800

    disable flash f and enable one-char hop

--- lua/keymap/editor.lua
+++ lua/keymap/editor.lua
@@ -49,8 +49,8 @@ local plug_map = {
 	["nv|<leader>w"] = map_cmd("<Cmd>HopWordMW<CR>"):with_noremap():with_desc("jump: Goto word"),
 	["nv|<leader>j"] = map_cmd("<Cmd>HopLineMW<CR>"):with_noremap():with_desc("jump: Goto line"),
 	["nv|<leader>k"] = map_cmd("<Cmd>HopLineMW<CR>"):with_noremap():with_desc("jump: Goto line"),
-	-- ["nv|<leader>c"] = map_cmd("<Cmd>HopChar1MW<CR>"):with_noremap():with_desc("jump: Goto one char"),
-	["nv|<leader>c"] = map_cmd("<Cmd>HopChar2MW<CR>"):with_noremap():with_desc("jump: Goto two chars"),
+	["nv|<leader>c"] = map_cmd("<Cmd>HopChar1MW<CR>"):with_noremap():with_desc("jump: Goto one char"),
+	-- ["nv|<leader>c"] = map_cmd("<Cmd>HopChar2MW<CR>"):with_noremap():with_desc("jump: Goto two chars"),
 
 	-- Plugin: nvim-spectre
 	["n|<leader>Ss"] = map_callback(function()
--- lua/modules/configs/editor/flash.lua
+++ lua/modules/configs/editor/flash.lua
@@ -21,7 +21,7 @@ return function()
 			-- options used when flash is activated through
 			-- `f`, `F`, `t`, `T`, `;` and `,` motions
 			char = {
-				enabled = true,
+				enabled = false,
 				-- hide after jump when not using jump labels
 				autohide = false,
 				-- show jump labels
