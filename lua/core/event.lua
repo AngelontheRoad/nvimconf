486fb1212409b99be82222824837bda0383a6461
AngelontheRoad <gjr2012477@gmail.com>
2025-12-10 13:13:02 +0800

    merge upstream

--- lua/core/event.lua
+++ lua/core/event.lua
@@ -56,6 +56,14 @@ vim.api.nvim_create_autocmd("LspAttach", {
 	end,
 })
 
+-- Start treesitter for installed parsers
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = require("core.settings").treesitter_deps,
+	callback = function(args)
+		vim.treesitter.start(args.buf)
+	end,
+})
+
 -- Autojump to last edit
 vim.api.nvim_create_autocmd("BufReadPost", {
 	callback = function()
