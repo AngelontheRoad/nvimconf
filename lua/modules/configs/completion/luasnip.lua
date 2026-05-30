b831286b4508156d15fc189c1e283978e4e0cea8
AngelontheRoad <gjr2012477@gmail.com>
2026-05-30 16:48:28 +0800

    update luasnip config

--- lua/modules/configs/completion/luasnip.lua
+++ lua/modules/configs/completion/luasnip.lua
@@ -1,6 +1,8 @@
 return function()
 	local vim_path = require("core.global").vim_path
-	local snippet_path = vim_path .. "/snips/"
+	local data_dir = require("core.global").data_dir
+	local snippet_path = vim_path .. "/snips"
+	local friendly_snippets_path = data_dir .. "lazy/friendly-snippets"
 
 	require("modules.utils").load_plugin("luasnip", {
 		keep_roots = true,
@@ -10,13 +12,16 @@ return function()
 		update_events = "TextChanged,TextChangedI",
 		delete_check_events = "TextChanged,InsertLeave",
 	}, false, require("luasnip").config.set_config)
+	require("luasnip").log.set_loglevel("info")
 
 	require("luasnip.loaders.from_vscode").lazy_load({
 		paths = {
 			snippet_path,
+			friendly_snippets_path,
 		},
 	})
-	-- load friendly-snippets
+	-- I don't know why the snippets from global.json appear twice
+	-- Adding line below to suppress
 	require("luasnip.loaders.from_vscode").lazy_load()
 	require("luasnip.loaders.from_lua").lazy_load()
 	require("luasnip.loaders.from_snipmate").lazy_load()
